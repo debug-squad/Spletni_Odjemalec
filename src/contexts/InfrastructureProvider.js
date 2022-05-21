b7048defcb5cf262bc8b64eae4573ab867badff8
Tim Prapotnik <tim.prapotnik@student.um.si>
2022-05-21 18:55:44 +0200

    Removed unused imports, Added initialize data for event and infrastructure

--- src/contexts/InfrastructureProvider.js
+++ src/contexts/InfrastructureProvider.js
@@ -1,4 +1,5 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useEffect, useState } from 'react'
+import { useAxiosState } from './AxiosProvider';
 
 export const InfrastructureContext = createContext({
     infrastructures: [],
@@ -7,11 +8,21 @@ export const InfrastructureContext = createContext({
 export const useInstractureState = () => useContext(InfrastructureContext);
 
 export function InfrastructureProvider({children}){
+    const { publicAxios } = useAxiosState();
+
     const [infrastructures, setInfrastructures] = useState([]);
     const value = {
         infrastructures,
         setInfrastructures: (infrastructures) => setInfrastructures(infrastructures),
     };
+
+    useEffect(()=> {
+        (async ()=> {
+            let req = await publicAxios.get('/infrastructure');
+            const infrastructures = req.data;
+            setInfrastructures(infrastructures);
+        })();
+    }, [publicAxios]);
 
     return (
        <InfrastructureContext.Provider value={value}>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,19 @@
 import { useClientState } from '../contexts/ClientProvider';
+import { useEventState } from '../contexts/EventProvider';
+import { useInstractureState } from '../contexts/InfrastructureProvider';
 
 export default function Info(){
     const {client, token} = useClientState();
+    const {events} = useEventState();
+    const {infrastructures} = useInstractureState();
 
     return(
         <>
             <p>Client: { JSON.stringify(client) } </p>
             <p>Token: { JSON.stringify(token) } </p>
+
+            <p>Events: { JSON.stringify(events) } </p>
+            <p>Infrastructures: { JSON.stringify(infrastructures) } </p>
         </>
     );
 }
