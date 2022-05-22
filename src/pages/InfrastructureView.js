ff77ba65d74e8a1181c8059968e7a85a72d5d4bf
Tim Prapotnik <tim.prapotnik@student.um.si>
2022-05-22 14:47:51 +0200

    Added so it gets up to date data,

--- src/pages/InfrastructureView.js
+++ src/pages/InfrastructureView.js
@@ -1,17 +1,26 @@
 import { useParams } from 'react-router-dom';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useInstractureState } from '../contexts/InfrastructureProvider';
+import { useAxiosState } from '../contexts/AxiosProvider';
 
 
 export default function InfrastructureView(){
+    const {publicAxios} = useAxiosState();
+
     const {id} = useParams();
     const {infrastructures} = useInstractureState();
-    const [infrastructure, setInfrastructure] = useState(infrastructures.filter(inf=>inf._id === id)[0]);
+    const [infrastructure, setInfrastructure] = useState(infrastructures.filter(inf=>inf._id === id)?.[0]);
 
-    return(
-        <>
-            <h1>{infrastructure.title}</h1>
-            <pre>{ JSON.stringify(infrastructure, 2, ' ') }</pre>
-        </>
-    );
+    useEffect(()=>{
+        (async ()=> {
+            const req = await publicAxios.get('/infrastructure/' + id);
+            const infrastructure = req.data;
+            setInfrastructure(infrastructure);
+        })()
+    }, [id]);
+
+    return (infrastructure ? <>
+        <h1>{infrastructure.title}</h1>
+        <pre>{ JSON.stringify(infrastructure, 2, ' ') }</pre>
+    </>: <span>Loading...</span>);
 }
