955b8f67cf2da9129c48011083f256657b30d35d
= <jan.sernec@gmail.com>
2022-05-23 01:40:55 +0200

    Graph

--- src/pages/Graph.js
+++ src/pages/Graph.js
@@ -51,8 +51,8 @@ export default function Graph() {
 			<div className="map-container">
 				<Filter />
 				<div className="map-view">
-					<Box sx={{ minWidth: 120,margin:"1em" }}>
-						<FormControl fullWidth color={"primary"} sx={{backgroundColor:"green"}}>
+					<Box sx={{ minWidth: 120,margin:"1em",}}>
+						<FormControl fullWidth sx={{backgroundColor: theme.palette.border.primary}}>
 							<InputLabel id="demo-simple-select-label">
 								Sort by attendance
 							</InputLabel>
