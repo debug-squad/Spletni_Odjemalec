1d5ed01d48f2806a6b0b1b054d2da53f0f8945b5
= <jan.sernec@gmail.com>
2022-05-22 20:25:23 +0200

    styles

--- src/components/AddEventConfig.js
+++ src/components/AddEventConfig.js
@@ -1,3 +1,4 @@
+import { Button } from "@mui/material";
 import { styled, useTheme, Box } from "@mui/system";
 import { useEffect, useState } from "react";
 import { useAxiosState } from "../contexts/AxiosProvider";
@@ -8,12 +9,10 @@ export default function AddEventConfig(props) {
     const ConfigBox = styled(Box)(({ theme }) => ({
 		padding: "10px",
 		position: "relative",
-		margin: "10px",
+		marginTop: "1em",
 		minHeight: "3em",
-		backgroundColor: theme.palette.background.menu,
-		// display: "flex",
-		/// alignItems: "center",
-		// justifyContent: "center",
+		backgroundColor: theme.palette.background.default,
+       
 	}));
 
     const [interval, setInterval] = useState(10);
@@ -21,6 +20,7 @@ export default function AddEventConfig(props) {
     const [loading, setLoading] = useState(false);
 
     const Save = async ()=> {
+        console.log("sdfdsfdsfdsf")
         setLoading(true);
         const req = await authAxios.post('/config-event', { interval, CSS_selector: CSSSelector });
         const data = req.data;
@@ -33,13 +33,13 @@ export default function AddEventConfig(props) {
     return (
         <>
             <ConfigBox>
-                <h4>Add new</h4>
+                <h3>Add new</h3>
                 <label>Interval</label><br/>
                 <input type="number" value={interval} onChange={e=>setInterval(+e.target.value)} placeholder="Interval seconds"/><br/>
                 <label>CSS Selektor</label><br/>
                 <input type="text" value={CSSSelector} onChange={e=>setCSSSelector(e.target.value)} placeholder="CSS Selector"/><br/>
                 <br/>
-                <button onClick={Save} disabled={loading}>Add</button>
+                <Button	variant="contained"	onClick={()=>{Save()}} disabled={loading}>Add</Button>
             </ConfigBox>
         </>
     );
