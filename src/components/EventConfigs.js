8daa716161c50ddd782cad9b56f7d1fd3eeb2ac6
= <jan.sernec@gmail.com>
2022-05-22 20:07:49 +0200

    Event Config into accordion

--- src/components/EventConfigs.js
+++ src/components/EventConfigs.js
@@ -2,10 +2,21 @@ import { useEffect, useState } from "react";
 import { useAxiosState } from "../contexts/AxiosProvider";
 import AddEventConfig from "./AddEventConfig";
 import EventConfig from "./EventConfig";
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 export default function EventConfigs(props) {
     const { authAxios } = useAxiosState();
     const [configs, setConfigs] = useState([]);
+
+    const [expanded, setExpanded] = React.useState(false);
+    const handleChange = (panel) => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false);
+    };
 
     useEffect(()=>{
         (async ()=>{
@@ -17,9 +28,20 @@ export default function EventConfigs(props) {
 
     return (
         <>
-            <h1>Event Config</h1>
-            { configs.map(config=><EventConfig key={config._id} config={config} onDelete={id => setConfigs(configs.filter(c=>c._id !== id))}/>)}
+            <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1bh-content"
+          id="panel1bh-header"
+        >
+          <Typography variant="h4" gutterBottom component="div">
+          Event Config						</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+        { configs.map(config=><EventConfig key={config._id} config={config} onDelete={id => setConfigs(configs.filter(c=>c._id !== id))}/>)}
             <AddEventConfig onChange={c=>setConfigs([...configs, c])}/>
+        </AccordionDetails>
+      </Accordion>
         </>
     );
 }
--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,13 +1,20 @@
-import AddAttendance from "../components/AddAttendande"
-import EventConfigs from "../components/EventConfigs"
+import { Typography } from "@mui/material";
+import AddAttendance from "../components/AddAttendande";
+import EventConfigs from "../components/EventConfigs";
 
-
-export default function Admin(){
-    return(
-        <>
-            <h2>Admin panel </h2>
-            <AddAttendance />
-            <EventConfigs />
-        </>
-        )
-} 
+export default function Admin() {
+	return (
+		<>
+			<Typography
+				variant="h3"
+				sx={{ textAlign: "center", margin: "1em" }}
+				gutterBottom
+				component="div"
+			>
+				Admin panel
+			</Typography>
+			<AddAttendance />
+			<EventConfigs sx={{ marginTop: "2em" }} />
+		</>
+	);
+}
