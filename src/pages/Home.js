cfa973a1822459ea75d811379b3744fd93c244ec
= <jan.sernec@gmail.com>
2022-05-24 20:36:18 +0200

    Added paddingBottom to Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,9 +7,9 @@ import Typography from "@mui/material/Typography";
 export default function Info() {
 	const { events } = useEventState();
 	return (
-        <Box sx={{ maxWidth: "100%", margin:"1em", marginBottom:"2em" }}>
+        <Box sx={{ maxWidth: "100%", margin:"1em"}}>
         <Typography variant="h4" gutterBottom component="div" sx={{textAlign:"center", margin:"1em"}}>All Events</Typography>
-            <Grid sx={{display:"flex",alignItems:"center",justifyContent:"center"}} container rowSpacing={3} columnSpacing={{ xs: 1, sm: 3, md: 4 }}>
+            <Grid sx={{display:"flex",alignItems:"center",justifyContent:"center",paddingBottom:"2em" }} container rowSpacing={3} columnSpacing={{ xs: 1, sm: 3, md: 4 }}>
 			{events.map((event) => (
                  <Grid item xs="auto">
 				<EventCard event={event} />
